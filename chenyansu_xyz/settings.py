54588b42e59ab1ed1e56cad30c66630e1e33e094
chenyansu <chenyansu@localhost.localdomain>
2018-03-27 23:44:26 +0800

    关闭调试,转移静态文件

--- chenyansu_xyz/settings.py
+++ chenyansu_xyz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%e_vp*_x^8odq8@$k+yd2qzq^)jc-*hg*2m=zr&x%e-be!#$w8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.chenyansu.xyz',
                 '111.230.64.149',
@@ -159,3 +159,6 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json' 
 CELERY_RESULT_BACKEND = 'django-db' 
 CELERY_TIMEZONE = 'Asia/Shanghai'
+
+#
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
