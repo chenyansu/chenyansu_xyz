15710edc528068ae14bff00e2a7694520205a933
chenyansu <chenyansu@localhost.localdomain>
2018-03-29 23:52:44 +0800

    url 向django2.0的形式转化

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,23 @@
 from django.conf.urls import url, include
+from django.urls import path, include, re_path
 from . import views
 from .feeds import LatestPostFeed
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    # url(r'^$', views.PostListView.as_view(),name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+    path('', views.post_list, name='post_list'),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail, name='post_detail'),
-    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
-    url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
-    url(r'^feed/$', LatestPostFeed(), name="post_feed"),
-    url(r'^search/$', views.post_search, name="post_search")
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostFeed(), name="post_feed"),
+    path('search/', views.post_search, name="post_search"),
+
+
+    # url(r'^$', views.PostListView.as_view(),name='post_list'),
+    # url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+    #     views.post_detail, name='post_detail'),
+    # url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    # url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    # url(r'^feed/$', LatestPostFeed(), name="post_feed"),
+    # url(r'^search/$', views.post_search, name="post_search")
 ]
--- chenyansu_xyz/urls.py
+++ chenyansu_xyz/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+
+from django.urls import path, include
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
@@ -24,8 +25,8 @@ sitemaps = {
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include(('blog.urls','blog' ),  namespace='blog' )),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps':sitemaps}, 
+    path('admin/', admin.site.urls),
+    path('blog/', include(('blog.urls','blog' ),  namespace='blog' )),
+    path('sitemap\.xml', sitemap, {'sitemaps':sitemaps}, 
         name='django.contrib.sitemaps.views.sitemap')
 ]
